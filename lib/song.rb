2c13d895f18ebdf6c33801f1d8817f8aacdcc2f8
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-28 14:29:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,16 +11,9 @@ class Song
   end
   
   def self.create
-    @@all << self.new
-    self
+    song=self.new
+    @@all << song
+    song
   end
-  
-  def self.new_by_name(name)
-    
-    
-  end
-  
-  
-    
 
 end
