291d0c106659e4eb7a58e141dcd78dca12964906
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-28 15:14:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    name=filename.split(/(( - )|[.])/).delete_at(0, 2).to_s
+    name=filename.split(/(( - )|[.])/).delete_at(0).delete_at(2)to_s
     song=self.new_by_name(name)
   end
 end
